7ddc8bed12def5348df3203df29a5e11042ee4f1
ElevatedPeace <99437895+ElevatedPeace@users.noreply.github.com>
2023-05-11 06:59:32 +0100

    new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import Movies from "./components/Movies"
+import React from "react";
+import Movies from "./components/Movies";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
-    <div>
-<Movies />
-    </div>
-  )
-}
+    
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/NotFound" element={<NotFound />} />
+      </Routes>
 
-export default App
+  );
+};
+
+export default App;
